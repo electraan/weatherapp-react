a0e90a133ea47605482bea7656b63cb6970919d8
electraan <electra.an@gmail.com>
2022-11-27 14:09:37 +0000

    added axios, display weather and spinner

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import Weather from "./weather";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <p>It`s owesome app!</p>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -15,8 +17,9 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React with me
         </a>
+        <Weather city="California" />
       </header>
     </div>
   );
---
+++ src/weather.js
@@ -0,0 +1,30 @@
+import React from "react";
+import axios from "axios";
+import { BallTriangle } from "react-loader-spinner";
+
+export default function Weather(props) {
+  function handleResponse(response) {
+    alert(
+      `The weather in ${response.data.name} is ${Math.round(
+        response.data.main.temp
+      )} °`
+    );
+  }
+
+  let key = "454b3c15e44c7f345644cf4c8c057675";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${props.city}&units=metric&lang=eng`;
+
+  axios.get(`${apiUrl}&appid=${key}`).then(handleResponse);
+  return (
+    <BallTriangle
+      height={80}
+      width={80}
+      radius={5}
+      color="#4fa94d"
+      ariaLabel="ball-triangle-loading"
+      wrapperClass={{}}
+      wrapperStyle=""
+      visible={true}
+    />
+  );
+}
