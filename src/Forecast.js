fd9dcc10b003948a4562cfcebb021eafd0369653
electraan <electra.an@gmail.com>
2022-12-25 15:51:16 +0000

    set daily forecast in Forecast

--- src/Forecast.js
+++ src/Forecast.js
@@ -1,12 +1,8 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Forecast.css";
 import axios from "axios";
 
-export default function Forecast(props) {
-  function showForecast(response) {
-    console.log(response.data);
-  }
-  /*function forecastDay(dayNum) {
+/*function forecastDay(dayNum) {
     let date = new Date(dayNum * 1000);
     let day = date.getDay();
     let weekDays = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
@@ -48,78 +44,89 @@ export default function Forecast(props) {
       }
     });
 */
-
-  let lat = props.coords.lat;
-  let lon = props.coords.lon;
-  let key = "454b3c15e44c7f345644cf4c8c057675";
-  let apiUrl = `https://api.openweathermap.org/data/3.0/onecall?lat=${lat}&lon=${lon}&appid=${key}&units=metric&lang=eng`;
-  axios.get(apiUrl).then(showForecast);
-
-  return (
-    <div className="col">
-      <div className="card-body border m-1 rounded">
-        <div className="weatherForecast">
-          <div className="forecast-day">
-            <div className="forecast-tempHigh">{props.name} C°</div>
-            <div className="forecast-tempLow">
-              <img
-                src="http://openweathermap.org/img/wn/03d@2x.png"
-                alt=""
-                width="25"
-              />
-              C°
+export default function Forecast(props) {
+  let [loaded, setLoaded] = useState(false);
+  let [dailyForecast, setDailyForecast] = useState(null);
+  function showForecast(response) {
+    setLoaded(true);
+    setDailyForecast(response.data.daily);
+    console.log(response.data.daily);
+  }
+  if (loaded) {
+    return (
+      <div className="col">
+        <div className="card-body border m-1 rounded">
+          <div className="weatherForecast">
+            <div className="forecast-day">
+              <div className="forecast-tempHigh">{props.name} C°</div>
+              <div className="forecast-tempLow">
+                <img
+                  src="http://openweathermap.org/img/wn/03d@2x.png"
+                  alt=""
+                  width="25"
+                />
+                C°
+              </div>
+              <hr />
             </div>
-            <hr />
-          </div>
-          <div className="forecast-day">
-            <div className="forecast-tempHigh">Day C°</div>
-            <div className="forecast-tempLow">
-              <img
-                src="http://openweathermap.org/img/wn/03d@2x.png"
-                alt=""
-                width="25"
-              />
-              C°
+            <div className="forecast-day">
+              <div className="forecast-tempHigh">Day C°</div>
+              <div className="forecast-tempLow">
+                <img
+                  src="http://openweathermap.org/img/wn/03d@2x.png"
+                  alt=""
+                  width="25"
+                />
+                C°
+              </div>
+              <hr />
             </div>
-            <hr />
-          </div>
-          <div className="forecast-day">
-            <div className="forecast-tempHigh">Day C°</div>
-            <div className="forecast-tempLow">
-              <img
-                src="http://openweathermap.org/img/wn/03d@2x.png"
-                alt=""
-                width="25"
-              />
-              C°
+            <div className="forecast-day">
+              <div className="forecast-tempHigh">Day C°</div>
+              <div className="forecast-tempLow">
+                <img
+                  src="http://openweathermap.org/img/wn/03d@2x.png"
+                  alt=""
+                  width="25"
+                />
+                C°
+              </div>
+              <hr />
             </div>
-            <hr />
-          </div>
-          <div className="forecast-day">
-            <div className="forecast-tempHigh">Day C°</div>
-            <div className="forecast-tempLow">
-              <img
-                src="http://openweathermap.org/img/wn/03d@2x.png"
-                alt=""
-                width="25"
-              />
-              C°
+            <div className="forecast-day">
+              <div className="forecast-tempHigh">Day C°</div>
+              <div className="forecast-tempLow">
+                <img
+                  src="http://openweathermap.org/img/wn/03d@2x.png"
+                  alt=""
+                  width="25"
+                />
+                C°
+              </div>
+              <hr />
             </div>
-            <hr />
-          </div>
-          <div className="forecast-day">
-            <div className="forecast-tempHigh">Day C°</div>
-            <div className="forecast-tempLow">
-              <img
-                src="http://openweathermap.org/img/wn/03d@2x.png"
-                alt=""
-                width="25"
-              />
-              C°
+            <div className="forecast-day">
+              <div className="forecast-tempHigh">Day C°</div>
+              <div className="forecast-tempLow">
+                <img
+                  src="http://openweathermap.org/img/wn/03d@2x.png"
+                  alt=""
+                  width="25"
+                />
+                C°
+              </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  } else {
+    let lat = props.coords.lat;
+    let lon = props.coords.lon;
+    let key = "454b3c15e44c7f345644cf4c8c057675";
+    let apiUrl = `https://api.openweathermap.org/data/3.0/onecall?lat=${lat}&lon=${lon}&appid=${key}&units=metric&lang=eng`;
+    axios.get(apiUrl).then(showForecast);
+
+    return `Loaded...`;
+  }
 }
